e5d4daf25cf7c38d30479cbc4b658797ab72169c
Manuel Campana <MANUEL.CAMPANA@local.ittcesena.it>
2025-10-31 09:08:59 +0100

    gitignore

---
+++ riproduzione musicale/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace riproduzione_musicale
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
