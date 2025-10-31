0a29976d119eb1aeec654c49e6fb31b4af7d8107
Stefano Cozzula <STEFANO.COZZULA@local.ittcesena.it>
2025-10-31 09:36:28 +0100

    R0.1: Prima vesione della classe brano con i metodi e il costruttore

--- riproduzione musicale/brano.cs
+++ riproduzione musicale/brano.cs
@@ -8,5 +8,59 @@ namespace riproduzione_musicale
 {
     internal class brano
     {
+        private string _titolo;
+        private string _autore;
+        private float _durata;
+
+        public brano(string titolo, string autore, float durata)
+        {
+
+            _titolo = titolo;
+            _autore = autore;
+            _durata = durata;
+
+        }
+
+        public string getTitolo()
+        {
+            return _titolo;
+        }
+
+        public string getautore()
+        {
+            return _autore;
+        }
+
+        public float getDurata()
+        {
+            return _durata;
+        }
+
+        public void setTitolo(string titolo)
+        {
+            _titolo = titolo;
+        }
+
+        public void setAutore(string autore)
+        {
+            _autore = autore;
+        }
+
+        public void setDurata(float durata)
+        {
+            _durata = durata;
+        }
+
+        public bool shortSong(float sogliaDurata)
+        {
+            if (_durata < sogliaDurata)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
