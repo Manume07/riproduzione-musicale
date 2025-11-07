97a9e3339a19e739af534a847b35616e6f4d01c2
Manume_ <clash41mini@gmail.com>
2025-11-07 15:39:02 +0100

    messo apposto la lista dei brani

--- riproduzione musicale/MainWindow.xaml.cs
+++ riproduzione musicale/MainWindow.xaml.cs
@@ -11,9 +11,10 @@ using System.Windows.Shapes;
 
 namespace riproduzione_musicale
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
+    
+    
+
     public partial class MainWindow : Window
     {
 
@@ -29,19 +30,20 @@ namespace riproduzione_musicale
             string titolo = txtTitolo.Text;
             string autore = txtAutore.Text;
             string durata = txtDurata.Text;
-            bool valore = false;
+            
 
 
             if ((string.IsNullOrEmpty(titolo) || string.IsNullOrEmpty(autore) || string.IsNullOrEmpty(durata))){
                 MessageBox.Show("ERRORE inserire tutti i brani");
                 txtTitolo.Clear();
                 txtAutore.Clear();
+                txtDurata.Clear();
               
             }
             else
-            {
+            {  
                 brano brano = new brano(titolo, autore, float.Parse(durata));
-                lstBrani.Items.Add(brano);
+                lstBrani.Items.Add($"Titolo: {titolo} - Autore: {autore} - (Durata: {durata} sec)");
             }
 
         }
